39f8b9bd15fe9471c5d05b748807f4e10b847cf8
goverdhan <pulse1@pulse.ooo>
2022-09-13 16:20:04 +0530

    rest apis

--- js/script.js
+++ js/script.js
@@ -12,17 +12,11 @@ $.ajax({
         console.log(resp);
 
     //    document.getElementById('empId1').innerHTML =  'Gov';
-
     var tbl = '';
-
     for(var i=0; i<resp.data.length; i++){
         tbl += '<tr><td>'+ resp.data[i].id + '</td><td>' + resp.data[i].employee_name + '</td><td>' + resp.data[i].employee_salary + '</td><td>' + resp.data[i].employee_age + '</td><td>' + resp.data[i].profile_image + '<td><button onClick="editRecord('+ resp.data[i].id +')">Edit</button><button onClick="deleteRecord(' + resp.data[i].id + ')">Delete</button></td></td></tr>';
     }
-
     document.getElementById("empTbl").innerHTML = tbl;
-
-
-
     },
     error: function(err){
         console.log(err)
@@ -30,21 +24,35 @@ $.ajax({
 });
 
 function editRecord(id){
-
-document.getElementById('editEmpForm').style.display = 'inline-block';
-document.getElementById('overlay').style.display = 'inline-block';
-
-$.ajax({
-    url: 'https://dummy.restapiexample.com/api/v1/employees/' + id,
-    method: 'GET',
-    dataType: 'JSON',
-    success: function(resp){
-        console.log(resp);
-    }
-});
-
-
+    document.getElementById('editEmpForm').style.display = 'inline-block';
+    document.getElementById('overlay').style.display = 'inline-block';
+    $.ajax({
+        url: 'https://dummy.restapiexample.com/api/v1/employee/' + id,
+        method: 'GET',
+        dataType: 'JSON',
+        success: function(resp){
+            console.log(resp);
+            document.getElementById('editEmpId').value = resp.data.id;
+            document.getElementById('editEmpName').value = resp.data.employee_name;
+            document.getElementById('editEmpAge').value = resp.data.employee_age;
+            document.getElementById('editEmpSalary').value = resp.data.employee_salary;
+        }
+    });
 }
+
+function submitEditForm(){
+    var id = document.getElementById('editEmpId').value || 1;
+    $.ajax({
+        url: 'https://dummy.restapiexample.com/api/v1/update/' + id,
+        method: 'PUT',
+        dataType: 'JSON',
+        data: {employee_name: document.getElementById('editEmpName').value, employee_age: document.getElementById('editEmpAge').value, employee_salary: document.getElementById('editEmpSalary').value},
+        success: function(resp){
+            closeEmpEditForm();
+        }
+    });
+}
+
 
 function closeEmpEditForm(){
     document.getElementById('editEmpForm').style.display = 'none';
@@ -52,7 +60,14 @@ function closeEmpEditForm(){
 }
 
 function deleteRecord(id){
-    alert(id);
+    $.ajax({
+        url: 'https://dummy.restapiexample.com/api/v1/delete/' + id,
+        method: 'DELETE',
+        dataType: 'JSON',
+        success: function(resp){
+            window.reload();
+        }
+    });
 }
 
 function addEmp(){
